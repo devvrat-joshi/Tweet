17b4e3dff68cb306d603eb0cbc485b8d0614e58e
devvrat-joshi <dsjoshi1990@gmail.com>
2020-11-13 23:12:43 +0530

    commit

--- client.py
+++ client.py
@@ -2,24 +2,20 @@ import socket
 import sys
 socket.setdefaulttimeout(3)
 class client:
-    def __init__(self,ip,port):
+    def __init__(self,ip,port,command):
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.sock.connect((ip,port))
-        self.sock.send(bytes("GET /page","utf-8"))
+        self.sock.send(bytes(command,"utf-8"))
         data = ""
         try:
             while 1:
                 k = self.sock.recv(10000).decode()
                 data += k
                 if not k:
-                    file = open("index.html","w")
-                    file.write(data)
-                    file.close()
                     break        
                 # print(data)
         except socket.timeout:
-            file = open("index.html","w")
-            file.write(data)
-            file.close()
-client("localhost",12345)
-print(sys.argv)
+            pass
+while 1:
+    command = input("Client : ")
+    client("localhost",12345,command)
