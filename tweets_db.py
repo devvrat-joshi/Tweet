5cbb0557b7ab76231d0c8af1d490827719931a47
yadav-sachin <sachinyadav7024@gmail.com>
2020-11-17 03:40:08 +0530

    parse tags from tweet

--- tweets_db.py
+++ tweets_db.py
@@ -26,11 +26,19 @@ c.execute("""
 
 conn.commit()
 
+def get_hastags(body):
+    body = body.split()
+    tags = []
+    for word in body:
+        if (word[0] == '#'):
+            tags.append(word[1: ])
+    return tags
+
 def post_tweet(username,body):
     try:
         if not body:
             return "The body does not exists."
-        c.execute("""
+        posted_tweet = c.execute("""
             INSERT INTO tweets (username, body)
             VALUES ('{}', '{}')
         """.format(username,body))
